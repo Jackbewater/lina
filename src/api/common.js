ac4f458f4afe04f0b0fc655a7d1a51e0c8092659
Jackbewater <1584328915@qq.com>
2025-07-04 15:15:03 +0800

    git commit -m '写了一些辣鸡代码'

--- src/api/common.js
+++ src/api/common.js
@@ -1,5 +1,19 @@
 import request from '@/utils/request'
 
+// 获取嵌入仪表板所需的token
+export function getToken() {
+  return request({
+    url: 'http://123.60.132.17:8080/api/token',
+    method: 'get'
+  }).then(response => {
+    // Ensure response.data exists and has a token property
+    if (!response.data || typeof response.data.token !== 'string') {
+      throw new Error('Invalid token response from API')
+    }
+    return response.data.token
+  })
+}
+
 export function createSourceIdCache(ids) {
   ids = ids.map(item => {
     if (typeof item === 'object' && item.id) {
--- src/views/iframe/IframePage.vue
+++ src/views/iframe/IframePage.vue
@@ -1,17 +1,61 @@
 <template>
-  <div>
-    <!-- Blank page for iframe content -->
+  <div class="card-iframe content-box">
+    <iframe id="iframe-dashboard-view" style="height: 80%; width: 80%; border: 0" src="" />
   </div>
 </template>
 
 <script>
+import { getToken } from '@/api/common'
+
 export default {
   name: 'IframePage',
   data() {
-    return {}
+    return {
+      dvId: '',
+      url: 'http://10.1.14.31:9080/#/chart-view',
+      params: {
+        busiFlag: 'dashboard',
+        dvId: '1122210916054732800',
+        type: 'Dashboard',
+        embeddedToken: '',
+        'de-embedded': true
+      },
+      iframe: null
+    }
+  },
+  mounted() {
+    this.iframe = document.getElementById('iframe-dashboard-view')
+
+    getToken()
+      .then(token => {
+        this.params.embeddedToken = token
+        if (this.iframe) {
+          this.iframe.src = this.url
+          window.addEventListener('message', this.onMessage, false)
+        }
+      })
+      .catch(error => {
+        console.error('Failed to get token:', error)
+        // Handle the error appropriately, e.g., show a user notification
+        this.$message.error('无法获取嵌入令牌: ' + error.message)
+      })
+  },
+  beforeDestroy() {
+    window.removeEventListener('message', this.onMessage, false)
+  },
+  methods: {
+    onMessage(event) {
+      if (event.data?.msgOrigin === 'de-fit2cloud' && this.iframe) {
+        const contentWindow = this.iframe.contentWindow
+        if (contentWindow) {
+          contentWindow.postMessage(this.params, '*')
+        }
+      }
+    }
   }
 }
 </script>
 
-<style scoped>
+<style>
+
 </style>
